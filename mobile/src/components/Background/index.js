c960ea5b15efa3d145dcaab91e1dcfc1a011d47b
George Santos <georgesantos@MacBook-Pro-de-George.local>
2020-01-31 22:52:19 -0300

    delete previous mobile folder

--- mobile/src/components/Background/index.js
+++
@@ -1,9 +0,0 @@
-import LinearGradient from 'react-native-linear-gradient';
-
-import styled from 'styled-components/native';
-
-export default styled(LinearGradient).attrs({
-  colors: ['#22202c', '#402845'],
-})`
-  flex: 1;
-`;
